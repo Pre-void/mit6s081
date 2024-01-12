bd2589637c95fc034fb89b325834083f0d91aaec
Pre-void <1301608557@qq.com>
2024-01-12 17:53:43 +0800

    "2024-01-12  pingpong"

---
+++ user/pingpong.c
@@ -0,0 +1,37 @@
+#include "../kernel/types.h"
+#include "user.h"
+
+int main(int argc,char * argv[]){
+    int pid;
+    int pipes1[2],pipes2[2];
+    pipe(pipes1);
+    pipe(pipes2);
+
+    int ret =fork();
+
+    if(ret == 0){
+        char child_send_buf[] = {'l'};
+        char child_recv_buf[] = {'0'};
+
+        pid = getpid();
+        close(pipes1[1]);
+        close(pipes2[0]);
+
+        read(pipes1[0],child_recv_buf,1);
+        printf("%d: reveived ping\n",pid);
+        write(pipes2[1],child_send_buf,1);
+        exit(0);
+    } else{
+        char parent_send_buf[] = {'z'};
+        char parent_recv_buf[] = {'0'};
+
+        pid = getpid();
+        close(pipes1[0]);
+        close(pipes2[1]);
+
+        write(pipes1[1],parent_send_buf,1);
+        read(pipes2[0],parent_recv_buf,1);
+        printf("%d: reveived pong\n",pid);
+        exit(0);
+    }
+}
--- user/sleep.c
+++ user/sleep.c
@@ -1,13 +1,14 @@
 #include "../kernel/types.h"
 #include "user.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(2, "usage: sleep [ticks num]\n");
+int main(int argc,char *argv[]){
+    /**判断参数是否有两个**/
+    if(argc != 2){
+        fprintf(2,"usage:sleep [tick num]\n");
         exit(1);
     }
-    // atoi sys call guarantees return an integer
+    /**转为整数**/
     int ticks = atoi(argv[1]);
-    int ret = sleep(ticks);
+    int ret  = sleep(ticks);
     exit(ret);
 }
