47e2c79593dd8f2335028a2c79aad62bd215369a
Pre-void <1301608557@qq.com>
2024-01-20 01:24:44 +0800

    "2024-01-19  sysinfo"

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -170,9 +170,9 @@ syscall(void)
   struct proc *p = myproc();
   /**获取系统调用号**/
   num = p->trapframe->a7;
-  if(num == 23){
-      printf("=================%d: syscall %s\n",p->pid,sysnames[num]);
-  }
+//  if(num == 23){
+//      printf("=================%d: syscall %s\n",p->pid,sysnames[num]);
+//  }
 
   /**检查系统调用号是否在合法范围内，以及系统调用是否存在**/
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
